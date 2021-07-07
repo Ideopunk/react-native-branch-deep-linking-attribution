6e743e05a9ee5235aaf77c12c495e18562dab4a8
Conor Barnes <conorbarnes93@gmail.com>
2021-07-07 14:56:12 -0400

    paste over expo-branch config plugin, install necessary dependencies

---
+++ plugin/src/withBranch.ts
@@ -0,0 +1,13 @@
+import { ConfigPlugin, createRunOncePlugin } from "@expo/config-plugins";
+import { withBranchAndroid } from "./withBranchAndroid";
+import { withBranchIOS } from "./withBranchIOS";
+
+const pkg = require("react-native-branch/package.json");
+
+const withBranch: ConfigPlugin = (config) => {
+	config = withBranchAndroid(config);
+	config = withBranchIOS(config);
+	return config;
+};
+
+export default createRunOncePlugin(withBranch, pkg.name, pkg.version);
